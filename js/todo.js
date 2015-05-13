47d1c3ae95c626f9567b2915f1f0892d24e84ed1
Jonathan Stallings <jonathanstallings@outlook.com>
2015-05-13 14:48:43 -0700

    Begin workon on todo.js

---
+++ js/todo.js
@@ -0,0 +1,8 @@
+//Goal: Add interactivity to Todo app
+
+//Declare Variables
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementById("add-button");
+var incompleteTasksHolder = document.querySelector("#incomplete-tasks ul");
+var completedTasksHolder = document.querySelector("#completed-tasks ul");
+
