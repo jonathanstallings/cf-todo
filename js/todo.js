2236cd5b5e9f5d6907eb89e87f5d8646457371d4
Jonathan Stallings <jonathanstallings@outlook.com>
2015-05-14 20:51:11 -0700

    Clean up js

--- js/todo.js
+++ js/todo.js
@@ -9,13 +9,11 @@ var addButton = document.getElementById("add-button");
 var incompleteTasksHolder = document.querySelector("#incomplete-tasks ul");
 var completedTasksHolder = document.querySelector("#completed-tasks ul");
 
-
 /*************************
 Functions
 **************************/
 
 var createNewTaskElement = function (taskString) {
-  //New Task List Item
   //Create necessary elements
   var listItem = document.createElement("li");
   var checkBox = document.createElement("input"); //checkbox
@@ -47,34 +45,29 @@ var createNewTaskElement = function (taskString) {
 
 var addTask = function () {
   //Add a new task
-  //Check to see if taskInput has a value
+
   if (taskInput.value) {
-    console.log("Add task. . .");
     //Create a new list item with the text from #new-task:
     var listItem = createNewTaskElement(taskInput.value);
 
     //Append listItem to incompleteTasksHolder
     incompleteTasksHolder.appendChild(listItem);
 
+    //Clear taskInput text
     taskInput.value = "";
-  } else {
-    //Do nothing if taskInput is empty
-    console.log("Empty taskInput");
   }
 };
 
 var editTask = function (el) {
   //Edit an existing task
-  console.log("Edit task. . .");
 
   var listItem = el.parentNode;
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
   var editButton = el;
-  var containsClass = listItem.classList.contains("editMode");
+  var editMode = listItem.classList.contains("editMode");
 
-  //If the class of the parent is .editMode
-  if (containsClass) {
+  if (editMode) {
     //Switch from .editMode
     //label text becomes input's value
     label.textContent = editInput.value;
@@ -91,7 +84,7 @@ var editTask = function (el) {
 
 var deleteTask = function (el) {
   //Delete an existing task
-  console.log("Delete task. . .");
+
   var listItem = el.parentNode;
   var ul = listItem.parentNode;
 
@@ -101,7 +94,7 @@ var deleteTask = function (el) {
 
 var taskCompleted = function (el) {
   //Mark a task as completed
-  console.log("Task completed. . .");
+
   //Append the task li to the #completed-tasks
   var listItem = el.parentNode;
   completedTasksHolder.appendChild(listItem);
@@ -109,7 +102,7 @@ var taskCompleted = function (el) {
 
 var taskIncomplete = function (el) {
   //Mark a task as incompleted
-  console.log("Task incomplete. . .");
+
   //Append the task li to the #incomplete-tasks
   var listItem = el.parentNode;
   incompleteTasksHolder.appendChild(listItem);
@@ -132,14 +125,14 @@ var taskEventHandler = function (e, checkBoxEventHandler) {
 };
 
 var addTaskEnterKeyEvent = function (e) {
-  //Capture enter key as click
+  //Capture enter key as addButton click
   if (e.keyCode == 13) {
     addButton.click();
   }
 };
 
 var editTaskEnterKeyEvent = function (e) {
-  //Capture enter key as click
+  //Capture enter key as editButton click
   var el = e.target;
   var editButton = el.parentNode.querySelector("button.edit");
   
@@ -149,7 +142,7 @@ var editTaskEnterKeyEvent = function (e) {
 };
 
 var dblclickEvent = function (e) {
-  //Body
+  //Capture double-click event as editButton click
   var el = e.target;
   var editButton = el.parentNode.querySelector("button.edit");
   
@@ -185,15 +178,3 @@ completedTasksHolder.addEventListener("click", function(e) {
 //Set double-click handlers on each task holder
 incompleteTasksHolder.addEventListener("dblclick", dblclickEvent);
 completedTasksHolder.addEventListener("dblclick", dblclickEvent);
-
-
-// //Set up event listener to call taskCompleted with event delegation
-// if (incompleteTasksHolder.addEventListener) { // If event listeners work
-//   incompleteTasksHolder.addEventListener('click', function(e) {
-//     taskCompleted(e);
-//   }, false);
-// } else { //Otherwise use old IE model
-//   incompleteTasksHolder.attachEvent('onclick', function(e) {
-//     taskCompleted(e);
-//   });
-// }
